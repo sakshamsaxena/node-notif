117185c97d3002e46470f6ee65dd6dbd73cf4908
Saksham Saxena <ontenth@live.com>
2017-04-02 20:59:56 +0530

    Cleaner Code. Ready to Go!

--- util/config.sample.js
+++ util/config.sample.js
@@ -1,18 +1,8 @@
 /* Configuration Data  */
 
-exports.smtp = {
-	host	: 'smtp.gmail.com',
-	port	: 465,
-	secure	: true,
-	auth: {
-		user: 'user@gmail.com',
-		pass: 'pass'
-	}
-};
-
 exports.db = {
 	url 		: "URL",
-	collection 	: "collection",
-	fieldName	: "Name",
-	operation	: "update"
+	collection 	: "COLLECTION",
+	fieldName	: "FIELDNAME",
+	operation	: "OPERATION"
 };
