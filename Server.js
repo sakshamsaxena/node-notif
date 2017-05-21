074aca82783e4e0f08e527806ddfde4f3a8091fc
Saksham Saxena <ontenth@live.com>
2017-05-21 16:48:39 +0530

    Minor fix

--- Server.js
+++ Server.js
@@ -13,10 +13,10 @@ var gpio = require('rpi-gpio');
 
 /* Get the current local IP and host it there */
 var ifaces = require('os').networkInterfaces();
-if(ifaces.wlan0 !== undefined && ifaces.wlan0[0].family == 'IPv4')
-	hostname = ifaces.wlan0[0].address;
-if(ifaces.eth0 !== undefined && ifaces.eth0[0].family == 'IPv4')
-	hostname = ifaces.eth0[0].address;
+if (ifaces.wlan0 !== undefined && ifaces.wlan0[0].family == 'IPv4')
+    hostname = ifaces.wlan0[0].address;
+if (ifaces.eth0 !== undefined && ifaces.eth0[0].family == 'IPv4')
+    hostname = ifaces.eth0[0].address;
 
 /* Middlewares and Config */
 const bodyParser = require('body-parser');
@@ -29,50 +29,48 @@ app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, 'views'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(bodyParser.urlencoded({
-	extended: true
+    extended: true
 }));
 
 /* Middlewares for Status */
 const limiter = new RateLimit({
-	windowMs: 60 * 60 * 1000,
-	max: 60,
-	delayMs: 0,
-	delayAfter: 0,
-	message: 'You have reached the maximum number of requests per hour. Please try again later.',
-	statusCode: 404
+    windowMs: 60 * 60 * 1000,
+    max: 60,
+    delayMs: 0,
+    delayAfter: 0,
+    message: 'You have reached the maximum number of requests per hour. Please try again later.',
+    statusCode: 404
 });
 
 /* GPIO Setup in RPi Schema (https://pinout.xyz) */
+gpio.setup(11, gpio.DIR_IN, gpio.EDGE_BOTH);
 
 /* Listen to change in sensors */
 gpio.on('change', function(channel, value) {
-	console.log("Something Changed");
-	// Write status to database
-	var data = {};
+    console.log("Something Changed");
+    // Write status to database
+    var data = {};
+    data.channel = channel;
+    data.value = value;
+    process.nextTick(function(data) {
+        // Trigger Push Notification
+        io.on('connection', function(socket) {
+            console.log("Connected.\n", data);
+            socket.broadcast.emit('SlotChange', data);
+        });
 
-	data.channel = channel;
-	data.value = value;
-
-	// Trigger Push Notification
-	io.on('connection', function(socket) {
-		console.log("Connected.\n", data);
-		socket.broadcast.emit('SlotChange', data);
-	});
-
-	io.close(function() {
-		console.log("Closed");
-	});
+        io.close(function() {
+            console.log("Closed");
+        });
+    });
 });
-
-	gpio.setup(11, gpio.DIR_IN, gpio.EDGE_BOTH);
-
 
 /* Home Route */
 app.get('/', function(req, res) {
-	res.render('Home');
+    res.render('Home');
 });
 
 /* Listen to Port 3000 */
 server.listen(3000, hostname, function() {
-	console.log("Pub Server is Live on "+hostname+":3000");
+    console.log("Pub Server is Live on " + hostname + ":3000");
 });
